e837ed3f4d9b5420072a9e7db3e6d65095072ecb
dspoka <dspoka@gmail.com>
2014-06-01 01:11:48 -0700

    Initial Commit

---
+++ KeepInTouch/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  KeepInTouch
+//
+//  Created by Daniel Spokoyny on 6/1/14.
+//  Copyright (c) 2014 Daniel Spokoyny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ KeepInTouch/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  KeepInTouch
+//
+//  Created by Daniel Spokoyny on 6/1/14.
+//  Copyright (c) 2014 Daniel Spokoyny. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
